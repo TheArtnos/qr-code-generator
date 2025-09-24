7e5f55e50a3913d447863ac83f69956eb0cfd8b0
TheArtnos <theartnos@gmail.com>
2025-09-24 13:10:09 +0330

    Update script.js

--- script.js
+++ script.js
@@ -45,7 +45,8 @@ document.addEventListener("DOMContentLoaded", () => {
 const generateQR = function (e) {
   e.preventDefault();
   if (textInput.value.length > 0) {
-    imageQr.src = `https://quickchart.io/qr?text=${textInput.value}&size=300`; // Set QR image
+    // imageQr.src = `https://quickchart.io/qr?text=${textInput.value}&size=300`; // Set QR image
+    imageQr.src = ` https://quickchart.io/qr?text=https%3A%2F%2Ftheartnos.github.io%2Fqr-code-generator%2Fwifi.html&dark=3a86ff&size=300`; // Set QR image
 
     downloadQr(imageQr.src); // Prepare download link
 
